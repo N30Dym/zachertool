39b381908d4a4551e3588f601016c1557a58efe6
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-07 22:37:34 +0200

    Doku ergänzt

--- app/Helpers/istSeiteVerfuegbar_helper.php
+++ app/Helpers/istSeiteVerfuegbar_helper.php
@@ -1,6 +1,15 @@
 <?php
 if (! function_exists('istSeiteVerfuegbar'))
 {
+	/**
+	 * ist Seite verfügbar
+	 *
+	 * Gibt an, ob die Seite verfügbar ist
+	 *
+	 * @param string $url 
+	 *
+	 * @return boolean
+	 */
 	function istSeiteVerfuegbar(string $url){
 		// Checke, ob eine valide URL übergeben wurde
 		if(!filter_var($url, FILTER_VALIDATE_URL)){
@@ -10,13 +19,13 @@ if (! function_exists('istSeiteVerfuegbar'))
 		// Initialisiere cURL
 		$curlInit = curl_init($url);
 		
-		// Optionen setzen
+		// Optionen für cURL setzen
 		curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
 		curl_setopt($curlInit,CURLOPT_HEADER,true);
 		curl_setopt($curlInit,CURLOPT_NOBODY,true);
 		curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
 
-		// Antwort bekommen
+		// Antwort holen
 		$antwort = curl_exec($curlInit);
 		
 		// Schließe die cURL Session

420ca22255baa04eaa5d175ba06911ad7670c766
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-08 14:02:13 +0200

    Dokumentation und Reihenfolge geändert

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -6,8 +6,9 @@
 <head>
     <title><?= esc($title) ?></title>
 	
-	
-	<!-- Lade bootstrap.min.css entweder aus dem Internet oder lokal -->
+<!----------------------------------------------------------------->	
+<!-- Lade bootstrap.min.css entweder aus dem Internet oder lokal -->
+<!----------------------------------------------------------------->
 		<?php if (istSeiteVerfuegbar("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css")) : ?>
 		
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -26,28 +27,9 @@
 		
 		<?php endif ?>
 		
-		
-	<!-- Lade bootstrap.min.js entweder aus dem Internet oder lokal -->	
-		<?php if (istSeiteVerfuegbar("https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js")) : ?>
-		
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-			
-		<?php else : ?>
-		
-			<?php if (! is_file(ROOTPATH . 'public\js\bootstrap.min.js')) : ?>
-			
-				<?php throw new \CodeIgniter\Exceptions\CriticalError('bootstrap.min.js'); ?>
-				
-			<?php else : ?>
-			
-				<script src="<?= base_url() ?>/public/js/bootstrap.min.js"></script>
-				
-			<?php endif ?>
-			
-		<?php endif ?>
-		
-		
-	<!-- Lade jquery.min.js entweder aus dem Internet oder lokal -->		
+<!----------------------------------------------------------------->	
+<!-- Lade jquery.min.js entweder aus dem Internet oder lokal     -->		
+<!----------------------------------------------------------------->
 		<?php if (istSeiteVerfuegbar("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js")) : ?>
 		
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -65,9 +47,29 @@
 			<?php endif ?>
 			
 		<?php endif ?>
-		
 
-		 
+<!----------------------------------------------------------------->
+<!-- Lade bootstrap.min.js entweder aus dem Internet oder lokal  -->	
+<!----------------------------------------------------------------->
+		<?php if (istSeiteVerfuegbar("https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js")) : ?>
+		
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+			
+		<?php else : ?>
+		
+			<?php if (! is_file(ROOTPATH . 'public\js\bootstrap.min.js')) : ?>
+			
+				<?php throw new \CodeIgniter\Exceptions\CriticalError('bootstrap.min.js'); ?>
+				
+			<?php else : ?>
+			
+				<script src="<?= base_url() ?>/public/js/bootstrap.min.js"></script>
+				
+			<?php endif ?>
+			
+		<?php endif ?>
+
+ 
  	<meta charset="UTF-8">
 	<meta name="description" content="<?= esc($description) ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">

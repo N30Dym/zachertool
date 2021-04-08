71c1dba4fdaf09d9db08d09a67b76b30963777d2
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-08 13:30:00 +0200

    Footer über gesamte breite, immer am Ende, schließt </main>

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,4 +1,11 @@
-<br \>
-	<em>&copy; <?php echo date('Y');?></em>
+</main>
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container-fluid">
+    <span class="text-muted"><em>&copy; <?php echo date('Y');?></em></span>
+  </div>
+</footer>
+	
+
+
 </body>
 </html>

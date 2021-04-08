253fc153db6b7362c421ae74c5df2d1f68e7b52c
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-08 13:35:07 +0200

    Idaflieg Logo hinzugefügt, Dropdown-Menüs erstellt

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,36 +1,54 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+      <img src="<?= base_url() ?>/public/bilder/Idaflieg_Logo_ohne_Text.jpg" alt="" height="35">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Piloten</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Flugzeuge</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Protokolle</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Admin</a>
-        </li>
-      </ul>
+        <li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
+			Piloten
+		  </a>
+		  <div class="dropdown-menu">
+			<a class="dropdown-item" href="#">Pilotenliste</a>
+			<a class="dropdown-item" href="#">Piloten hinzufügen</a>
+		  </div>
+		</li>
+        <li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
+			Flugzeuge
+		  </a>
+		  <div class="dropdown-menu">
+			<a class="dropdown-item" href="#">Flugzeugliste</a>
+			<a class="dropdown-item" href="#">Flugzeug hinzufügen</a>
+			<a class="dropdown-item" href="#">Muster hinzufügen</a>
+		  </div>
+		</li>
+        <li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown"  style="cursor:pointer;">
+			Protokolle
+		  </a>
+		  <div class="dropdown-menu">
+			<a class="dropdown-item" href="#">Neues Protokoll</a>
+			<a class="dropdown-item" href="#">Offene Protokolle</a>
+			<a class="dropdown-item" href="#">Fertige Protokolle</a>
+		  </div>
+		</li>
 	      <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Dropdown link
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Link 1</a>
-        <a class="dropdown-item" href="#">Link 2</a>
-        <a class="dropdown-item" href="#">Link 3</a>
-      </div>
-    </li>
-  </ul>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle disabled" id="navbardrop" data-toggle="dropdown">
+			Admin
+		  </a>
+		  <div class="dropdown-menu">
+			<a class="dropdown-item" href="#">Link 1</a>
+			<a class="dropdown-item" href="#">Link 2</a>
+			<a class="dropdown-item" href="#">Link 3</a>
+		  </div>
+		</li>
+	  </ul>
     </div>
   </div>
 </nav>

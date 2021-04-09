64e16dab957155815c8beb85b29fa0b230411157
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-09 15:20:51 +0200

    getFlugzeugeDiesesJar() aufgeräumt

--- app/Models/flugzeuge/FlugzeugeModel.php
+++ app/Models/flugzeuge/FlugzeugeModel.php
@@ -6,27 +6,28 @@ use CodeIgniter\Model;
 
 class FlugzeugeModel extends Model
 {
-	######################################################################
-	### Initialisieren der Datenbank und Zugriff auf Tabelle flugzeuge ###
-	######################################################################
+######################################################################
+### Initialisieren der Datenbank und Zugriff auf Tabelle flugzeuge ###
+######################################################################
     protected $DBGroup = 'flugzeugeDB';
 	protected $table      = 'flugzeuge';
     protected $primaryKey = 'id';
 	protected $createdField  = 'erstelltAm';
+
 	
-	public function getFlugzeugeDiesesJahr($anzahl = 0)
+	
+	public function getFlugzeugeDiesesJahr()
 	{
-		##############################################################################
-		### Query zum Anzeigen der Kennzeichen die im letzten Jahr geflogen wurden ###
-		##############################################################################
-		$query = "SELECT DISTINCT zachern_flugzeuge.flugzeuge.kennung FROM zachern_protokolle.protokolle LEFT JOIN zachern_flugzeuge.flugzeuge ON zachern_protokolle.protokolle.flugzeugID = zachern_flugzeuge.flugzeuge.id WHERE YEAR(zachern_protokolle.protokolle.datum) = YEAR(CURDATE())-1";
-		if($anzahl == 0)
-		{
-
-			return $this->query($query)->getResult();
-
-		}
 		
-		return $this->findAll($anzahl);
+	##############################################################################
+	### Query zum Anzeigen der Kennzeichen die im letzten Jahr geflogen wurden ###
+	##############################################################################
+
+		$query = "SELECT DISTINCT zachern_flugzeuge.flugzeuge.kennung FROM zachern_protokolle.protokolle LEFT JOIN zachern_flugzeuge.flugzeuge ON zachern_protokolle.protokolle.flugzeugID = zachern_flugzeuge.flugzeuge.id WHERE YEAR(zachern_protokolle.protokolle.datum) = YEAR(CURDATE())-1";
+		return $this->query($query)->getResult();
+
+
+		
+
 	}
 }

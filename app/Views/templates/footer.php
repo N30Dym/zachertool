707ab73f5505fbccf859b8f0478d8073157b3877
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-09 23:13:57 +0200

    Klassenänderungen um das Aussehen anzupassen

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -2,11 +2,11 @@
 <footer class="footer mt-auto py-3 bg-light">
 	<div class="container-fluid">
 		<div class="row mx-3 flex-nowrap justify-content-between align-items-center">
-		  <div class="col-6">
-			<span class="text-muted"><em>&copy; <?php echo date('Y');?></em></span>
+		  <div class="col-6 d-flex">
+			<span class="text-muted"><em>&copy; <?= date('Y') ?></em></span>
 		  </div>
 		  <div class="col-6 d-flex justify-content-end text-end">
-			<span class="text-muted">Design erstellt mit Bootstrap</em></span>
+			<span class="text-muted">Designt mit Bootstrap</em></span>
 		  </div>
 		</div>
 	</div>

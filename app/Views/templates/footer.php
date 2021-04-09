506a741d0e94879c74946c7d3d0f5500a032b0a4
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-09 15:10:59 +0200

    Design überarbeitet, <main> jetzt in navbar

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,8 +1,16 @@
 </main>
 <footer class="footer mt-auto py-3 bg-light">
-  <div class="container-fluid">
-    <span class="text-muted"><em>&copy; <?php echo date('Y');?></em></span>
-  </div>
+	<div class="container-fluid">
+		<div class="row mx-3 flex-nowrap justify-content-between align-items-center">
+		  <div class="col-6">
+			<span class="text-muted"><em>&copy; <?php echo date('Y');?></em></span>
+		  </div>
+		  <div class="col-6 d-flex justify-content-end text-end">
+			<span class="text-muted">Design erstellt mit Bootstrap</em></span>
+		  </div>
+		</div>
+	</div>
+	
 </footer>
 	
 
--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand navbar-light bg-light">
+<nav class="navbar navbar-expand navbar-light bg-secondary bg-gradient ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="">
       <img src="<?= base_url() ?>/public/bilder/Idaflieg_Logo_ohne_Text.jpg" alt="" height="35">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
-		  <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
+		  <a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
 			Piloten
 		  </a>
 		  <div class="dropdown-menu">
@@ -18,17 +18,23 @@
 		  </div>
 		</li>
         <li class="nav-item dropdown">
-		  <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
+		  <a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
 			Flugzeuge
 		  </a>
 		  <div class="dropdown-menu">
 			<a class="dropdown-item" href="#">Flugzeugliste</a>
 			<a class="dropdown-item" href="#">Flugzeug hinzufügen</a>
+			<?php if (isset($_SESSION["admin"]) && $SESSION["admin"] = TRUE) :?>
+				<?= '<a class="dropdown-item" href="#">Flugzeug bearbeiten</a>' ?>
+			<?php endif ?>
 			<a class="dropdown-item" href="#">Muster hinzufügen</a>
+			<?php if (isset($_SESSION["admin"]) && $SESSION["admin"] = TRUE) :?>
+				<?= '<a class="dropdown-item" href="#">Muster bearbeiten</a>' ?>
+			<?php endif ?>
 		  </div>
 		</li>
         <li class="nav-item dropdown">
-		  <a class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown"  style="cursor:pointer;">
+		  <a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown"  style="cursor:pointer;">
 			Protokolle
 		  </a>
 		  <div class="dropdown-menu">
@@ -39,7 +45,7 @@
 		</li>
 	      <!-- Dropdown -->
 		<li class="nav-item dropdown">
-		  <a class="nav-link dropdown-toggle disabled" id="navbardrop" data-toggle="dropdown">
+		  <a class="nav-link dropdown-toggle disabled text-white-50" id="navbardrop" data-toggle="dropdown">
 			Admin
 		  </a>
 		  <div class="dropdown-menu">
@@ -50,5 +56,9 @@
 		</li>
 	  </ul>
     </div>
+	<div class="d-flex justify-content-end align-content-center">
+	      <img src="<?= base_url() ?>/public/bilder/DLR-Signet_grau.jpg" alt="" height="35">
+	</div>	  
   </div>
 </nav>
+<main class="container bg-light shadow p-3 mb-5 rounded">

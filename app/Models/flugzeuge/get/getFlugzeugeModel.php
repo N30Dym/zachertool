6bb3df0772f7ed3b5756f935bb4c5223c6d7cf9d
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-11 16:33:11 +0200

    Pfad und Namen an die neue Struktur angepasst

--- app/Models/flugzeuge/get/getFlugzeugeModel.php
+++ app/Models/flugzeuge/get/getFlugzeugeModel.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace App\Models\flugzeuge;
+namespace App\Models\flugzeuge\get;
 
 use CodeIgniter\Model;
 
-class FlugzeugeModel extends Model
+class getFlugzeugeModel extends Model
 {
-######################################################################
-### Initialisieren der Datenbank und Zugriff auf Tabelle flugzeuge ###
-######################################################################
+	/*
+	 * Verbindungsvariablen für den Zugriff zur
+	 * Datenbank zachern_protokolle auf die 
+	 * Tabelle protokolle
+	 */
     protected $DBGroup = 'flugzeugeDB';
 	protected $table      = 'flugzeuge';
     protected $primaryKey = 'id';
@@ -16,7 +18,7 @@ class FlugzeugeModel extends Model
 
 	
 	
-	public function getProtokolleDiesesJahr()
+	/*public function getProtokolleDiesesJahr()
 	{
 		
 	##############################################################################
@@ -25,10 +27,31 @@ class FlugzeugeModel extends Model
 
 		$query = "SELECT DISTINCT zachern_flugzeuge.flugzeuge.kennung, zachern_flugzeuge.muster.musterName FROM zachern_flugzeuge.flugzeuge JOIN zachern_protokolle.protokolle ON zachern_protokolle.protokolle.flugzeugID = zachern_flugzeuge.flugzeuge.id JOIN zachern_flugzeuge.muster ON zachern_flugzeuge.flugzeuge.musterID = zachern_flugzeuge.muster.id WHERE YEAR(zachern_protokolle.protokolle.datum) = YEAR(CURDATE())-1;";
 		return $this->query($query)->getResult();	
-	}
+	}*/
 	
-	public function getAnzahlProtokolleProFlugzeug($flugzeugID)
+	/*public function getAnzahlProtokolleProFlugzeug($flugzeugID)
 	{
 		return $flugzeugID;
+	}*/
+	
+	/*
+	* Diese Funktion ruft nur das Protokoll mit
+	* der jeweiligen ID auf
+	*
+	* @param  mix $id int oder string
+	* @return object
+	*/
+	public function getFlugzeugeNachID($id)
+	{			
+		if(is_int(trim($id)) OR is_numeric(trim($id)))
+		{
+			$query = "SELECT * FROM flugzeuge WHERE id = ". trim($id);
+			return $this->query($query)->getResult();	
+		}
+		else
+		{
+			// Fehler beim übergebenen Wert
+			throw new BadMethodCallException('Call to undefined method ' . $className . '::' . $name);
+		}
 	}
 }

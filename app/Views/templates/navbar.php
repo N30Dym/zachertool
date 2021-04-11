89ac8e168bc6ae0bbced06ea60ddb0cd64ffea46
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-11 14:45:52 +0200

    Login-Link hinzugefügt, Spacing und div-ing überarbeitet

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,66 +1,78 @@
+</head>
+<body class="white">
 <header>
 	<nav class="navbar navbar-expand bg-secondary bg-gradient ">
-	  <div class="container-fluid">
-		<a class="navbar-brand" href="">
-		  <img src="<?= base_url() ?>/public/bilder/Idaflieg_Logo_ohne_Text.jpg" alt="" height="35">
-		</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		  <span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse flex" id="navbarNav">
-		  <ul class="navbar-nav">
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
-				Piloten
-			  </a>
-			  <div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Pilotenliste</a>
-				<a class="dropdown-item" href="#">Piloten hinzufügen</a>
-			  </div>
-			</li>
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
-				Flugzeuge
-			  </a>
-			  <div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Flugzeugliste</a>
-				<a class="dropdown-item" href="#">Flugzeug hinzufügen</a>
-				<?php if (isset($_SESSION["admin"]) && $SESSION["admin"] = TRUE) :?>
-					<?= '<a class="dropdown-item" href="#">Flugzeug bearbeiten</a>' ?>
-				<?php endif ?>
-				<a class="dropdown-item" href="#">Muster hinzufügen</a>
-				<?php if (isset($_SESSION["admin"]) && $SESSION["admin"] = TRUE) :?>
-					<?= '<a class="dropdown-item" href="#">Muster bearbeiten</a>' ?>
-				<?php endif ?>
-			  </div>
-			</li>
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown"  style="cursor:pointer;">
-				Protokolle
-			  </a>
-			  <div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Neues Protokoll</a>
-				<a class="dropdown-item" href="#">Offene Protokolle</a>
-				<a class="dropdown-item" href="#">Fertige Protokolle</a>
-			  </div>
-			</li>
-			  <!-- Dropdown -->
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle disabled text-white-50" id="navbardrop" data-toggle="dropdown">
-				Admin
-			  </a>
-			  <div class="dropdown-menu">
-				<a class="dropdown-item" href="#">Link 1</a>
-				<a class="dropdown-item" href="#">Link 2</a>
-				<a class="dropdown-item" href="#">Link 3</a>
-			  </div>
-			</li>
-		  </ul>
+		<div class="container-fluid">
+			<a class="navbar-brand" href="">
+				<img class="rounded-1" src="<?= base_url() ?>/public/bilder/Idaflieg_Logo_ohne_Text.jpg" alt="" height="35">
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse  navbar-collapse flex" id="navbarNav">
+				<ul class="navbar-nav col-md-6">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
+							Piloten
+						</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Pilotenliste</a>
+							<a class="dropdown-item" href="#">Piloten hinzufügen</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown" style="cursor:pointer;">
+							Flugzeuge
+						</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Flugzeugliste</a>
+							<a class="dropdown-item" href="#">Flugzeug hinzufügen</a>
+							
+							<?php if (isset($_SESSION["admin"]) && $SESSION["admin"] = TRUE) :?>
+								<a class="dropdown-item" href="#">Flugzeug bearbeiten</a>
+							<?php endif ?>
+							
+							<a class="dropdown-item" href="#">Muster hinzufügen</a>
+							
+							<?php if (isset($_SESSION["admin"]) && $SESSION["admin"] = TRUE) :?>
+								<a class="dropdown-item" href="#">Muster bearbeiten</a>
+							<?php endif ?>
+							
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle text-white" id="navbardrop" data-toggle="dropdown"  style="cursor:pointer;">
+							Protokolle
+						</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Neues Protokoll</a>
+							<a class="dropdown-item" href="#">Offene Protokolle</a>
+							<a class="dropdown-item" href="#">Fertige Protokolle</a>
+						</div>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle disabled text-white-50" id="navbardrop" data-toggle="dropdown">
+							Admin
+						</a>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#">Link 1</a>
+							<a class="dropdown-item" href="#">Link 2</a>
+							<a class="dropdown-item" href="#">Link 3</a>
+						</div>
+					</li>
+				</ul>
+				<div class="navbar-nav col-md-6 d-flex flex-row-reverse pe-3">
+						<a class="nav-link align-baseline text-white float-end" href="">
+							Login
+						</a>	
+
+				</div>
+			</div>
+			<a class="navbar-brand" href="">
+				<img class="rounded-1" src="<?= base_url() ?>/public/bilder/DLR-Signet_grau.jpg" alt="" height="35">
+			</a>
+				
 		</div>
-		<div class="d-flex justify-content-end align-content-center">
-			  <img src="<?= base_url() ?>/public/bilder/DLR-Signet_grau.jpg" alt="" height="35">
-		</div>	  
-	  </div>
 	</nav>
 </header>
 <main class="container bg-light shadow p-3 mb-5 rounded">

6eae13b33a62b9b1d55c6dd83e92445627078b00
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-11 16:51:59 +0200

    Erstmal zum laufen gebracht

--- app/Controllers/flugzeuge/flugzeugNeu.php
+++ app/Controllers/flugzeuge/flugzeugNeu.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\flugzeuge;
 
 use CodeIgniter\Controller;
+use App\Models\muster\get\getMusterModel;
 
 class flugzeugNeu extends Controller
 {
@@ -17,6 +18,9 @@ class flugzeugNeu extends Controller
 			throw new \CodeIgniter\Exceptions\PageNotFoundException('startseite.php');
 		}
 		
+		$getMusterModel = new getMusterModel();
+		$alleMuster = $getMusterModel->getAlleMuster();
+		
 		$title = "Musterauswahl";		
 		$description = "Wähle das Muster des Flugzeuges aus, dass du hinzufügen möchtest";
 		
@@ -24,10 +28,14 @@ class flugzeugNeu extends Controller
 			"title" => $title,
 			"description" => "Das webbasierte Tool zur Zacherdatenverarbeitung"
 		];
+		$dataContent = [
+			'flugzeuge' => $alleMuster,
+		];
+		//var_dump($alleMuster);
 		
-		echo view('templates/header', $dataHeader);
+		echo view('templates/header',  $dataHeader);
 		echo view('templates/navbar');
-		echo view('flugzeuge/musterauswahl');
+		echo view('flugzeuge/musterauswahl', $dataContent);
 		echo view('templates/footer');
 	}
 	
---
+++ app/Views/flugzeuge/musterauswahl.php
@@ -0,0 +1,15 @@
+	<?php if(count($flugzeuge) == 0): ?>
+		<div class="text-center">
+			Die Verbindung ist fehlgeschlagen
+		</div>
+	<?php else: ?>
+	<table class="table table-light table-striped table-hover border rounded">
+		
+		<?php foreach($flugzeuge as $flugzeug_item) : ?>
+			
+			<tr class="text-center">
+				<td><?= esc($flugzeug_item->musterName) ?></td>
+			</tr>
+			
+		<?php endforeach ?>
+	<?php endif ?>

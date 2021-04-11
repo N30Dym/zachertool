1fd9c1998c44f91cee7a3546239e4b4b08c208c1
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-11 14:49:20 +0200

    footer an Seitenende fixiert

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,12 +1,12 @@
 </main>
-<footer class="footer mt-auto py-3 bg-light">
+<footer class="footer fixed-bottom mt-auto py-3 bg-light">
 	<div class="container-fluid">
 		<div class="row mx-3 flex-nowrap justify-content-between align-items-center">
 		  <div class="col-6 d-flex">
 			<span class="text-muted"><em>&copy; <?= date('Y') ?></em></span>
 		  </div>
 		  <div class="col-6 d-flex justify-content-end text-end">
-			<span class="text-muted">Designt mit Bootstrap</em></span>
+			<span class="text-muted">Designed mit Bootstrap</em></span>
 		  </div>
 		</div>
 	</div>

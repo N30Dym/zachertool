96ac6bb17eddd25a043adf1ad0a34c8379d24fa9
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-12 14:28:31 +0200

    Klassennamen angepasst (war noch der alte)

--- app/Models/muster/get/getMusterDetailsModel.php
+++ app/Models/muster/get/getMusterDetailsModel.php
@@ -4,7 +4,7 @@ namespace App\Models\muster\get;
 
 use CodeIgniter\Model;
 
-class getMusterModel extends Model
+class getMusterDetailsModel extends Model
 {
 	/*
 	 * Verbindungsvariablen für den Zugriff zur

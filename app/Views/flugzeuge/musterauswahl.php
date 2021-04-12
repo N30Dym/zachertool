6237f74b58a09f9426534a9a84597f06398fb280
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-12 14:13:15 +0200

    Funktionierende Version

--- app/Views/flugzeuge/musterauswahl.php
+++ app/Views/flugzeuge/musterauswahl.php
@@ -1,15 +1,27 @@
-	<?php if(count($flugzeuge) == 0): ?>
+<h2 class="text-center"><?= esc($title) ?></h2>	
+<div class="row">
+	<div class="col-3">
+		<span class="pt-5 sticky-top">
+			Wenn es das Muster noch nicht gibt kannst du <a href="">hier</a> ein neues anlegen.
+		</span>
+	</div>
+	<div class="col-6 d-flex justify-content-center">	
+	<?php if(count($muster) == 0): ?>
 		<div class="text-center">
 			Die Verbindung ist fehlgeschlagen
 		</div>
 	<?php else: ?>
 	<table class="table table-light table-striped table-hover border rounded">
 		
-		<?php foreach($flugzeuge as $flugzeug_item) : ?>
-			
+		<?php foreach($muster as $muster_item) : ?>
 			<tr class="text-center">
-				<td><?= esc($flugzeug_item["musterName"]) ?></td>
-			</tr>
-			
+					<td>
+			<a href="<?= base_url() ?>/flugzeuge/flugzeugNeu/<?= esc($muster_item["id"]) ?>">
+				<?= esc($muster_item["musterSchreibweise"]) ?><?= esc($muster_item["musterZusatz"]) ?>
+			</a>
+</td>
+				</tr>
 		<?php endforeach ?>
 	<?php endif ?>
+	</div>
+</div>

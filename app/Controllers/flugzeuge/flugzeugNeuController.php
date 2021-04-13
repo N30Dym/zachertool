3887bc650e1fb103130aceca412ce51b0752a34c
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-13 14:22:20 +0200

    Zwischenstand gespeichert

--- app/Controllers/flugzeuge/flugzeugNeuController.php
+++ app/Controllers/flugzeuge/flugzeugNeuController.php
@@ -5,11 +5,19 @@ namespace App\Controllers\flugzeuge;
 use CodeIgniter\Controller;
 use App\Models\muster\get\getMusterModel;
 use App\Models\muster\get\getMusterDetailsModel;
+use App\Models\muster\get\getMusterHebelarmeModel;
+use App\Models\muster\get\getMusterWoelbklappenModel;
 helper("array");
 
 class flugzeugNeuController extends Controller
 {
-	
+	/*
+	* Diese Funktion wird ausgeführt wenn in der URL folgender Pfad aufgerufen wird (siehe Routes.php):
+	* -> /flugzeuge/flugzeugNeu
+	*
+	* Sie lädt das View: flugzeuge/musterAuswahl.php, lädt alle verfügbaren Muster und
+	* stellt diese zur Auswahl zur Verfügung.
+	*/
 	public function index()
 	{
 		//Auswahl eines Muster, Link um neues Muster zu erstellen
@@ -39,31 +47,127 @@ class flugzeugNeuController extends Controller
 		$title = "Musterauswahl";		
 		$description = "Wähle das Muster des Flugzeuges aus, dass du hinzufügen möchtest";
 		
-		$dataHeader = [
+		$datenHeader = [
 			"title" => $title,
 			"description" => "Das webbasierte Tool zur Zacherdatenverarbeitung"
 		];
-		$dataContent = [
+		$datenInhalt = [
 			'muster' => $alleMuster
 		];
 		
-		echo view('templates/headerView',  $dataHeader);
+		echo view('templates/headerView',  $datenHeader);
 		echo view('templates/navbarView');
-		echo view('flugzeuge/musterauswahlView', $dataContent);
+		echo view('flugzeuge/musterauswahlView', $datenInhalt);
 		echo view('templates/footerView');
 	}
 	
+	/*
+	* Diese Funktion wird ausgeführt wenn in der URL folgender Pfad aufgerufen wird (siehe Routes.php):
+	* -> /flugzeuge/flugzeugNeu/(:num) bzw. -> /flugzeuge/flugzeugNeu/neu
+	*
+	* Sie lädt das View: flugzeuge/flugzeugAngabenView.php und übergibt die Daten, um ein neues Flugzeug
+	* bzw. ein neues Flugzeug sammt neuem Muster zu erstellen.
+	* 
+	* @param $musterID wird automatisch aus der URL entnommen
+	*/
+	
 	public function flugzeugAnlegen($musterID)
 	{
+		
+		/*
+		* -> /flugzeuge/flugzeugNeu/$musterID
+		* Wenn in der URL eine musterID übergeben wurde, wird diese Bedingung ausgeführt.
+		* Es werden alle Tabelleneinträge der Tabellen muster, muster_details, muster_hebelarme 
+		* und muster_klappen geladen, die mit der musterID Korrellieren. Die Arrays werden, dann 
+		* in das Array $datenInhalt geladen um es anschließend dem flugzeugAngabenView zu übergeben.
+		*
+		*/
 		if($musterID)
 		{
+			// Alles aus Tabelle zachern_flugzeuge.muster als Array laden, wo die id = $musterID
+			$getMusterModel = new getMusterModel();
+			$muster = $getMusterModel->getMusterNachID($musterID);
+			
+			// Alles aus Tabelle zachern_flugzeuge.muster_details als Array laden, wo die musterID = $musterID
 			$getMusterDetailsModel = new getMusterDetailsModel();
 			$musterDetails = $getMusterDetailsModel->getMusterDetailsNachMusterID($musterID);
 			var_dump($musterDetails);
+			
+			// Alles aus Tabelle zachern_flugzeuge.muster_hebelarme als Array laden, wo die musterID = $musterID
+			$getMusterHebelarmeModel = new getMusterHebelarmeModel();
+			$musterHebelarme = $getMusterHebelarmeModel->getMusterHebelarmeNachID($musterID);
+			
+			// $musterWoelbklappen initialisieren und leer setzten, falls kein WK-Flugzeug, bleibt die Variable leer
+			$musterWoelbklappen = null;
+			
+			// Prüfen, ob das Muster ein WK-Flugzeug ist
+			if($muster["woelbklappen"])
+			{
+				// Wenn ja, alles aus Tabelle zachern_flugzeuge.muster_klappen als Array laden, wo die musterID = $musterID
+				$getMusterWoelbklappenModel = new getMusterWoelbklappenModel();
+				$musterWoelbklappen = $getMusterWoelbklappenModel->getMusterWoelbklappenNachID($musterID);
+			}
+			
+			$title = "Flugzeug des Musters ". $muster["musterSchreibweise"] . $muster["musterZusatz"] ." anlegen";
+			
+			// Die Variable $datenInhalt mit den geladenen Arrays bestücken
+			$datenInhalt = [
+			'muster' => $muster,
+			'musterDetails' => $musterDetails,
+			'musterHebelarme' => $musterHebelarme,
+			'musterWoelbklappen' => $musterWoelbklappen
+			];
 		}
+		
+		/*
+		* Wenn in der URL keine musterID übergeben wurde, sondern "neu", wird diese Bedingung ausgeführt.
+		* Es werden alle Tabelleneinträge der Tabellen muster, muster_details, muster_hebelarme 
+		* und muster_klappen geladen, die mit der musterID Korrellieren. Die Arrays werden, dann 
+		* in das Array $datenInhalt geladen um es anschließend dem flugzeugAngabenView zu übergeben.
+		*
+		*/
 		else
 		{
-			echo "Neu";
+			$title = "Neues Flugzeug und Flugzeugmuster erstellen";
+			
+			// Alle Felder der Arrays leer setzen, um nicht im View überprüfen zu müssen ob die Variable gesetzt ist oder nicht
+			
+			// Aus Tabelle zachern_flugzeuge.muster leere Einträge als Array laden
+			$getMusterModel = new getMusterModel();
+			$musterLeer = $getMusterModel->getMusterLeer();
+			
+			// Aus Tabelle zachern_flugzeuge.muster_details leere Einträge als Array laden
+			$getMusterDetailsModel = new getMusterDetailsModel();
+			$musterDetailsLeer = $getMusterDetailsModel->getMusterDetailsLeer();
+			
+			// Aus Tabelle zachern_flugzeuge.muster_hebelarme leere Einträge als Array laden
+			$getMusterHebelarmeModel = new getMusterHebelarmeModel();
+			$musterHebelarmeLeer = $getMusterHebelarmeModel->getMusterHebelarmeLeer();
+			
+			// Null, da beim neuen Muster noch nicht bekannt, ob WK oder nicht
+			$musterWoelbklappenLeer = null;
+			
+			// Die Variable $datenInhalt mit den geladenen Arrays bestücken
+			$datenInhalt = [
+			'muster' => $musterLeer,
+			'musterDetails' => $musterDetailsLeer,
+			'musterHebelarme' => $musterHebelarmeLeer,
+			'musterWoelbklappen' => $musterWoelbklappenLeer
+			];
 		}
+		
+		
+		// Daten für den HeaderView initialisieren
+		$datenHeader = [
+			"title" => $title,
+			"description" => "Das webbasierte Tool zur Zacherdatenverarbeitung"
+		];	
+		
+		
+		// Views aufrufen, Seite laden und Daten übertragen
+		echo view('templates/headerView',  $datenHeader);
+		echo view('templates/navbarView');
+		echo view('flugzeuge/flugzeugAngabenView', $datenInhalt);
+		echo view('templates/footerView');		
 	}
 }

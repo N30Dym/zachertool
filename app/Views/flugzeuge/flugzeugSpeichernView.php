87dde43ee6ace9c62605ee7f774ee49cf24f5b1e
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-16 21:11:53 +0200

    methode zu post geändert

--- app/Views/flugzeuge/flugzeugSpeichernView.php
+++ app/Views/flugzeuge/flugzeugSpeichernView.php
@@ -1,3 +1,10 @@
-		</form>	
+<h2 class="text-center m-4"><?= esc($title) ?></h2>	
+
+<?= \Config\Services::validation()->listErrors() ?>
+
+<div class="row">
+	<div class="col-2">
 	</div>
-</div>
+	<div class="col-8">
+	
+		<form class="needs-validation" method="post" action="flugzeugSpeichern" novalidate=""><!--  novalidate="" nur zum testen!! -->

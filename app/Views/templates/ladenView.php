971ab467d7712ae6319d70ecaf72ae20d8f5f080
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-16 22:01:25 +0200

    Initialer Upload

---
+++ app/Views/templates/ladenView.php
@@ -0,0 +1,9 @@
+</head>
+<body class="bg-light">
+<main>
+
+	<div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">	
+		<div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+			<span class="visually-hidden">Loading...</span>
+		</div>
+	</div>

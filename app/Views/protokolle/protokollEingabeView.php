4c06ab89aea963e2fadd2d90d948cb96ce5936fc
Eisbaer-PC <lars.kastner@dlr.de>
2021-04-21 23:16:31 +0200

    Spielerei

--- app/Views/protokolle/protokollEingabeView.php
+++ app/Views/protokolle/protokollEingabeView.php
@@ -3,9 +3,23 @@
 
 <div class="row">
 	<div class="col-2">
+		<div class="p-3 row bg-dark">
+		
+			<input type="submit" class="col-12" value="1. Einleitung" formaction="/">
+			<input type="submit" class="col-12" value="2. Einleitung" formaction="/">
+			<input type="submit" class="col-12" value="3. Einleitung" formaction="/">
+			<input type="submit" class="col-12" value="4. Einleitung" formaction="/">
+		
+		</div>	
 	</div>
 	<div class="col-8">
 	
 		<!--<form class="needs-validation" method="post" action="/flugzeuge/flugzeugNeu/15" novalidate=""><!--  novalidate="" nur zum testen!! -->
 		<?=  form_open(site_url('flugzeuge/flugzeugNeu/flugzeugSpeichern'), ["class" => "needs-validation", /*"novalidate" => "novalidate"*/]) ?>
 
+
+
+
+	<div class="col-2">
+	</div>
+</div>

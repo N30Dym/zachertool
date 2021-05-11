8c1a9cca84c8389696077445f2ee95625817908f
Eisbaer-PC <lars.kastner@dlr.de>
2021-05-11 20:49:15 +0200

    Datepicker für Safari Browser hinzugefügt

--- app/Views/protokolle/scripts/protokollErsteSeiteScript.php
+++ app/Views/protokolle/scripts/protokollErsteSeiteScript.php
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script type="text/javascript">    
 $( document ).ready( function() {
 
@@ -14,6 +16,17 @@ $( document ).ready( function() {
     $( document ).on( 'click', '#kapitelAuswahl', function(){
        $( '#kapitelGo' ).attr('formaction', '<?= site_url('/protokolle/kapitel/') ?>' + $( this ).val());
     });
+    
+    if ( /^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+        $('input[type=date], .datepicker').keydown(function() { return false });
+        $('input[type=date], .datepicker').datepicker({
+          //yearRange: "-80:0",
+          dateFormat: 'dd.mm.yyyy',
+          changeMonth: true,
+          changeYear: true,
+          firstDay: 1,
+        });
+    }
 
 });
 </script>

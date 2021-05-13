3d89d939fff2c269965cfd18a135895371d96954
Eisbaer-PC <lars.kastner@dlr.de>
2021-05-13 13:42:12 +0200

    <div>s mit Einheiten hinzugefügt

--- app/Views/piloten/pilotenEingabeView.php
+++ app/Views/piloten/pilotenEingabeView.php
@@ -27,7 +27,7 @@ td {
 
                 <div class="col-sm-4">
                     <label for="spitzname" class="form-label ms-2"><b>Spitzname</b></label>
-                    <input type="text" class="form-control" name="pilot[spitzname]" value="<?= isset($pilotArray['spitzname']) ? esc($pilotArray['spitzname']) : (isset($spitzname) ? esc($spitzname) : "") ?>" required <?= isset($pilotID) ? "readonly" : "" ?>>
+                    <input type="text" class="form-control" name="pilot[spitzname]" value="<?= isset($pilotArray['spitzname']) ? esc($pilotArray['spitzname']) : (isset($spitzname) ? esc($spitzname) : "") ?>" <?= isset($pilotID) ? "readonly" : "" ?>>
                 </div>
 
                 <div class="col-sm-4">
@@ -37,7 +37,10 @@ td {
 
                 <div class="col-12">
                     <label for="groesse" class="form-label ms-2"><b>Größe</b></label>
-                    <input type="text" class="form-control" name="pilot[groesse]" value="<?= isset($pilotArray['groesse']) ? esc($pilotArray['groesse']) : (isset($groesse) ? esc($groesse) : "") ?>" required <?= isset($pilotID) ? "readonly" : "" ?>>
+                    <div class="input-group">
+                        <input type="number" min="0" step="1" class="form-control" name="pilot[groesse]" value="<?= isset($pilotArray['groesse']) ? esc($pilotArray['groesse']) : (isset($groesse) ? esc($groesse) : "") ?>" required <?= isset($pilotID) ? "readonly" : "" ?>>
+                        <span class="input-group-text">cm</span>
+                    </div>
                 </div>
                 
                 <div class="table-responsive">
@@ -55,20 +58,20 @@ td {
                         <?php foreach($pilotDetailsArray as $pilotDetail) : ?>
                             <tr>
                             <td><input type="date" class="form-control" min="0" value="<?= $pilotDetail['datum'] ?>" disabled></td>
-                            <td><input type="number" class="form-control" name="pilotDetail[stundenNachSchein]" min="0" value="<?= $pilotDetail['stundenNachSchein'] ?>" disabled></td>
-                            <td><input type="number" class="form-control" name="pilotDetail[geflogeneKm]" min="0" value="<?= $pilotDetail['geflogeneKm'] ?>" disabled></td>
+                            <td><div class="input-group"><input type="number" class="form-control" name="pilotDetail[stundenNachSchein]" min="0" step="1" value="<?= $pilotDetail['stundenNachSchein'] ?>" disabled><span class="input-group-text">h</span></div></td>
+                            <td><div class="input-group"><input type="number" class="form-control" name="pilotDetail[geflogeneKm]" min="0" value="<?= $pilotDetail['geflogeneKm'] ?>" disabled><span class="input-group-text">km</span></div></td>
                             <td><input type="number" class="form-control" name="pilotDetail[typenAnzahl]" min="0" value="<?= $pilotDetail['typenAnzahl'] ?>" disabled></td>
-                            <td><input type="number" class="form-control" name="pilotDetail[gewicht]" min="0" value="<?= $pilotDetail['gewicht'] ?>" disabled></td>
+                            <td><div class="input-group"><input type="number" class="form-control" name="pilotDetail[gewicht]" min="0" value="<?= $pilotDetail['gewicht'] ?>" disabled><span class="input-group-text">kg</span></div></td>
                         </tr>
                         <?php endforeach ?>
                     <?php endif ?>
                         <tfoot>
                             <tr>
                                 <?php if(isset($pilotID)) : ?><td class="text-end"><b>Neu:</b></td><?php endif ?>
-                                <td><input type="number" class="form-control" name="pilotDetail[stundenNachSchein]" min="0" value="<?= isset($stundenNachSchein) ? esc($stundenNachSchein) : "" ?>" required></td>
-                                <td><input type="number" class="form-control" name="pilotDetail[geflogeneKm]" min="0" value="<?= isset($geflogeneKm) ? esc($geflogeneKm) : "" ?>" required></td>
+                                <td><div class="input-group"><input type="number" class="form-control" name="pilotDetail[stundenNachSchein]" min="0" value="<?= isset($stundenNachSchein) ? esc($stundenNachSchein) : "" ?>" required><span class="input-group-text">h</span></div></td>
+                                <td><div class="input-group"><input type="number" class="form-control" name="pilotDetail[geflogeneKm]" min="0" value="<?= isset($geflogeneKm) ? esc($geflogeneKm) : "" ?>" required><span class="input-group-text">km</span></div></td>
                                 <td><input type="number" class="form-control" name="pilotDetail[typenAnzahl]" min="0" value="<?= isset($typenAnzahl) ? esc($typenAnzahl) : "" ?>" required></td>
-                                <td><input type="number" class="form-control" name="pilotDetail[gewicht]" min="0" value="<?= isset($gewicht) ? esc($gewicht) : "" ?>" required></td>
+                                <td><div class="input-group"><input type="number" class="form-control" name="pilotDetail[gewicht]" min="0" value="<?= isset($gewicht) ? esc($gewicht) : "" ?>" required><span class="input-group-text">kg</span></div></td>
                             </tr>
                         </tfoot>
                     </table>

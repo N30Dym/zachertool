ef32a070d1ccf36a98c2774b7597188f99143d5a
Eisbaer-PC <lars.kastner@dlr.de>
2021-05-28 14:35:43 +0200

    Session starten und falls in der URL nicht protokolle steht, wird ['protokoll'] gelöscht

--- app/Views/templates/headerView.php
+++ app/Views/templates/headerView.php
@@ -18,6 +18,18 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<?php
 
+    if(session_status() !== PHP_SESSION_ACTIVE)
+    {
+        $session = session();    
+    }
+    
+    if(isset($_SESSION['protokoll']) && !strpos(current_url(), 'protokolle'))
+    {
+        unset($_SESSION['protokoll']);
+    }
+        
+?>
 
 <!-- </head> befindet sich in navbarView.php um die Möglichkeit zu lassen mit einem weiteren View noch <script>-Code einzufügen -->

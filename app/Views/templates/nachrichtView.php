a7b36055708eaea70f25c01c662dc15fa8d4d516
Eisbaer-PC <lars.kastner@dlr.de>
2021-06-14 00:07:37 +0200

    <div class=min-vh-100 h-100> hinzugefügt, um den Footer nach unten zu schieben

--- app/Views/templates/nachrichtView.php
+++ app/Views/templates/nachrichtView.php
@@ -2,11 +2,11 @@
 </head>
 <body class="bg-light">
 <main>
-
-	<div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);" class="text-center">	
-		<h3><?= esc($nachricht) ?></h3>
-		<a href="<?= esc($link) ?>"
-			<button type="button" class="btn btn-primary">Zurück</button>
-		</a>
-	</div>
+    <div class="min-vh-100 h-100"></div>
+    <div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);" class="text-center">	
+            <h3><?= esc($nachricht) ?></h3>
+            <a href="<?= esc($link) ?>"
+                    <button type="button" class="btn btn-primary">Zurück</button>
+            </a>
+    </div>
 	

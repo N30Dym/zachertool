5565981dfae24797eff8396cdf1ef797012e057c
Eisbaer-PC <lars.kastner@dlr.de>
2021-06-19 00:20:40 +0200

    Datenbank Migration

---
+++ app/Database/Migrations/2021-06-18-214617_ZachernPilotenPiloten.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ZachernPiloten extends Migration
+{
+    protected $DBGroup = 'pilotenDB';
+    
+    public function up()
+    {
+        if ($this->forge->createDatabase('zachern_piloten', TRUE))
+        {
+            echo 'Database created!';
+        }
+        
+        $this->forge->addField([
+            'id' => [
+                'type'              => 'INT',
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'vorname' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'null'              => false,
+            ],
+            'spitzname' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'null'              => true,
+                'default'           => null,
+            ],
+            'nachname' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'null'              => false,
+            ],
+            'akafliegID' => [
+                'type'              => 'INT',
+                'constraint'        => '5',
+                'null'              => true,
+            ],
+            'groesse' => [
+                'type'              => 'INT',
+                'constraint'        => '5',
+                'null'              => true,
+            ],
+            'sichtbar' => [
+                'type'              => 'TINYINT',
+                'constraint'        => '1',
+                'null'              => true,
+                'default'           => '1',
+            ],
+            'zachereinweiser' => [
+                'type'              => 'TINYINT',
+                'constraint'        => '1',
+                'null'              => true,
+                'default'           => null,
+            ],
+            'erstelltAm' => [
+                'type'              => 'DATETIME',
+                'null'              => false,
+                'default'           => 'CURRENT_TIMESTAMP()',
+            ],
+            'geaendertAm' => [
+                'type'              => 'DATETIME',
+                'null'              => false,
+                'default'           => 'CURRENT_TIMESTAMP()',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('piloten');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('piloten');
+    }
+}
---
+++ app/Database/Migrations/2021-06-18-221446_ZachernPilotenAkafliegs.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ZachernPilotenAkafliegs extends Migration
+{
+    public function up()
+    {
+        if ($this->forge->createDatabase('zachern_piloten', TRUE))
+        {
+            echo 'Database created!';
+        }
+        
+        $this->forge->addField([
+            'id' => [
+                'type'              => 'INT',
+                'unsigned'          => true,
+                'auto_increment'    => true,
+            ],
+            'akaflieg' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => '255',
+                'null'              => false,
+            ],
+            'sichtbar' => [
+                'type'              => 'TINYINT',
+                'constraint'        => '1',
+                'null'              => true,
+                'default'           => '1',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('piloten_akafliegs');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('piloten_akafliegs');
+    }
+}

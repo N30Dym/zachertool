63b903939ffac3ce439afd36b094f459caab4fd5
Eisbaer-PC <lars.kastner@dlr.de>
2021-06-19 18:00:41 +0200

    Angefangen mit ForeignKeys

---
+++ app/Database/Migrations/2021-06-19-154035_ZachernPilotenSetzeforeignkeys.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class ZachernPilotenSetzeforeignkeys extends Migration
+{
+    protected $DBGroup = 'pilotenDB';
+    
+    public function up()
+    {
+        $this->db->query("ALTER TABLE `piloten` ADD CONSTRAINT `fk_piloten_akafliegs` FOREIGN KEY(`akafliegID`) REFERENCES `piloten_akaflieg`(`id`) ON ON DELETE SET NULL ON UPDATE CASCADE;");
+    }
+
+    public function down()
+    {
+        $this->forge->dropForeignKey('tablename','users_foreign');
+    }
+}

d17c703baffe8e64813fbb5f9dc29c7eb1a623c7
Lars Kastner <lars.kastner@dlr.de>
2021-08-16 19:05:20 +0200

    Zusatztext für Beladungseingabe geändert

--- app/Views/protokolle/protokollKapitelID3View.php
+++ app/Views/protokolle/protokollKapitelID3View.php
@@ -18,8 +18,8 @@
     <?php else: ?>
         
         <div class="col-12 alert alert-secondary">
-            <small>Bitte die gesamte Beladung angegeben. Das Feld für das Fallschirmsgewicht darf nicht leer bleiben, kann aber mit "0" angegeben werden.
-                Zusatzgewicht im jeweiligen Sitz kann z.B. ein Bleikissen sein.</small>
+            <small>Die Felder für das Piloten- und Fallschirmgewicht dürfen nicht leer bleiben. Wenn ohne Fallschirm geflogen wird, kann '0' eingegeben werden. Andere Felder können leer bleiben
+                Zusatzgewicht im jeweiligen Sitz kann z.B. ein Bleikissen sein. </small>
         </div>
         <div class="table-responsive-lg">
             <table class="table">

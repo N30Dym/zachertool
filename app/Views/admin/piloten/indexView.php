56de7f7d7a681e4361f7375ed9752fbdf4f7cb6b
Lars Kastner <lars.kastner@dlr.de>
2021-08-21 18:21:09 +0200

    Links umgebaut, sodass der Pfad in der Klammer von base_url steht

--- app/Views/admin/piloten/indexView.php
+++ app/Views/admin/piloten/indexView.php
@@ -9,18 +9,18 @@
         <div class="p-3 m-3 rounded shadow border">
             <h3 class="m-2">Pilotenlisten</h3>
             <ul style="list-style-type:none">
-                <li><a href="<?= base_url() ?>/piloten/liste">Liste aller sichtbaren Piloten</a></li>
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/sichtbarePiloten">Sichtbare Piloten anzeigen und Sichtbarkeit ändern</a></li>
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/unsichtbarePiloten">Unsichtbare Piloten anzeigen und Sichtbarkeit ändern</a></li>
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/pilotenLoeschen">Piloten löschen</a></li>
+                <li><a href="<?= base_url('/piloten/liste') ?>">Liste aller sichtbaren Piloten</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/sichtbarePiloten') ?>">Sichtbare Piloten anzeigen und Sichtbarkeit ändern</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/unsichtbarePiloten') ?>">Unsichtbare Piloten anzeigen und Sichtbarkeit ändern</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/pilotenLoeschen') ?>">Piloten löschen</a></li>
             </ul>
         </div>
                        
         <div class="p-3 m-3 rounded shadow border">
             <h3 class="m-2">Akafliegs</h3>
             <ul style="list-style-type:none">
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/akafliegsAnzeigen">Akafliegs anzeigen und Sichtbarkeit ändern</a></li>
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/akafliegHinzufügen">Neue Akaflieg hinzufügen</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/akafliegsAnzeigen') ?>">Akafliegs anzeigen und Sichtbarkeit ändern</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/akafliegHinzufügen') ?>">Neue Akaflieg hinzufügen</a></li>
             </ul>
         </div>
         
@@ -31,8 +31,8 @@
         <div class="p-3 m-3 rounded shadow border">
             <h3 class="m-2">Zachereinweiser</h3>
             <ul style="list-style-type:none">
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/einweiserAnzeigen">Zachereinweiser anzeigen</a></li>
-                <li><a href="<?= base_url() ?>/admin/piloten/liste/einweiserAuswählen">Zachereinweiser wählen aus sichtbaren Piloten</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/einweiserAnzeigen') ?>">Zachereinweiser anzeigen</a></li>
+                <li><a href="<?= base_url('/admin/piloten/liste/einweiserAuswählen') ?>">Zachereinweiser wählen aus sichtbaren Piloten</a></li>
             </ul>
         </div>
         

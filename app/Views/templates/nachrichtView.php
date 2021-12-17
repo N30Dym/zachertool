9beded863aed8fb48a0c4f06fc5ea2a83a4df939
Lars Kastner <lars.kastner@dlr.de>
2021-12-17 15:09:05 +0100

    Da es keinen Footer auf der Nachrichtenseite mehr gibt, hier die Tags main, body und html geschlossen

--- app/Views/templates/nachrichtView.php
+++ app/Views/templates/nachrichtView.php
@@ -4,9 +4,13 @@
 <main>
     <div class="min-vh-100 h-100"></div>
     <div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);" class="text-center">	
-            <h3><?= esc($nachricht) ?></h3>
-            <a href="<?= esc($link) ?>"
-                    <button type="button" class="btn btn-primary">Zurück</button>
-            </a>
+        <h3><?= esc($nachricht) ?></h3>
+        <a href="<?= esc($link) ?>"
+            <button type="button" class="btn btn-primary">Zurück</button>
+        </a>
     </div>
+    
+</main>
+</body>
+</html>
 	

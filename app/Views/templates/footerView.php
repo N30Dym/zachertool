3c48c163d73ac47d3d803d7f30ca0c3f6460dec2
Lars Kastner <lars.kastner@dlr.de>
2021-12-17 15:07:49 +0100

    Indents entfernt, DLR Logo mit Link hinzugefügt, DLR Text hinzugefügt, '/em' entfernt

--- app/Views/templates/footerView.php
+++ app/Views/templates/footerView.php
@@ -3,20 +3,21 @@
 </main>
 <div style="height:3rem !important"></div>
 <footer class="footer mt-auto py-2 bg-gradient" style="background-color: #343a40;">
-	<div class="container-fluid">
-		<div class="row mx-3 flex-nowrap justify-content-between align-items-center">
-		  <div class="col-6 d-flex">
-			<span class="text-white-50"><em>&copy; <?= date('Y') ?></em></span>
-		  </div>
-		  <div class="col-6 d-flex justify-content-end text-end">
-                      <span class="text-white-50">Designed mit <a href="https://getbootstrap.com/" class="link-light" target="_blank">Bootstrap</a></em></span>
-		  </div>
-		</div>
-	</div>
+    <div class="container-fluid">
+        <div class="row mx-3 flex-nowrap justify-content-between align-items-center">
+            <div class="col-6 d-flex">
+                <a href="https://www.dlr.de">
+                    <img src="<?= base_url('/public/bilder/DLRSignetinWeißalsPNG.png') ?>" alt="" height="20">
+                </a>
+                <span class="text-white-50 ps-2">&copy; <em><?= date('Y') ?>, Deutsches Zentrum für Luft- und Raumfahrt e. V.</em></span>
+            </div>
+            <div id="footer" class="col-6 d-flex justify-content-end text-end">
+                <span class="text-white-50">Designed mit <a href="https://getbootstrap.com/" class="link-light" target="_blank">Bootstrap</a></span>
+            </div>
+        </div>
+    </div>
 	
 </footer>
-	
-
 
 </body>
 </html>

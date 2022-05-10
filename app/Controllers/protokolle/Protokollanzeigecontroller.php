29650794587fd21376afd6d1493cbd65dc4d379e
Lars Kastner <lars.kastner@dlr.de>
2022-05-10 12:46:07 +0200

    Überarbeitung und Doku fertig

--- app/Controllers/protokolle/Protokollanzeigecontroller.php
+++ app/Controllers/protokolle/Protokollanzeigecontroller.php
@@ -2,9 +2,21 @@
 
 namespace App\Controllers\protokolle;
 
+/**
+ * Child-Klasse vom ProtokollController. Übernimmt das Laden des Front-Ends für alle protokollbezogenen Seiten.
+ *
+ * @author Lars Kastner
+ */
 class Protokollanzeigecontroller extends Protokollcontroller
 {
-    protected function ladeErsteSeiteView($datenHeader, $datenInhalt)
+    
+    /**
+     * Gibt alle benötigten Views aus, um die erste Seite der Protokolleingabe anzuzeigen.
+     * 
+     * @param array $datenHeader
+     * @param array $datenInhalt
+     */
+    protected function ladeErsteSeiteView(array $datenHeader, array $datenInhalt)
     {
         echo view('templates/headerView', $datenHeader);
         echo view('protokolle/scripts/protokollErsteSeiteScript');
@@ -14,7 +26,13 @@ class Protokollanzeigecontroller extends Protokollcontroller
         echo view('templates/footerView');
     }
     
-    protected function ladeProtokollEingabeView($datenHeader, $datenInhalt)
+    /**
+     * Gibt alle benötigten Views aus, um die benötigt werden, um die einzelnen Kapitel der Protokolleingabe anzuzeigen.
+     * 
+     * @param array $datenHeader
+     * @param array $datenInhalt
+     */
+    protected function ladeProtokollEingabeView(array $datenHeader, array $datenInhalt)
     {             
         echo view('templates/headerView', $datenHeader);
         echo view('protokolle/scripts/protokollEingabeScript');
@@ -28,29 +46,32 @@ class Protokollanzeigecontroller extends Protokollcontroller
         echo view('templates/footerView');  
     }
     
-    protected function ladeWeitereViews($datenInhalt)
+    /**
+     * Gibt je nach aktueller protokollKapitelID einen entsprechenden View zurück.
+     * 
+     * Wenn das aktuelle Kapitel eine protokollKapitelID hat, die mit einer der globalen Variablen FLUGZEUG_EINGABE,
+     * PILOT_EINGABE oder BELADUNG_EINGABE übereinstimmt (siehe /Config/Constants.php), wird die entsprechende statische Anzeige geladen.
+     * Sonst wird die dynamische Anzeige 'protokollKapitelView' mit Inhalten der Datenbank geladen.
+     * Wenn die Seite 'BELADUNG_EINGABE' geladen wird, wird außerdem der Beladungszustand aus dem Zwischenspeicher gelöscht.
+     * 
+     * @param array $datenInhalt
+     */
+    protected function ladeWeitereViews(array $datenInhalt)
     {
         switch($_SESSION['protokoll']['kapitelIDs'][$_SESSION['protokoll']['aktuellesKapitel']])
         {
             case FLUGZEUG_EINGABE:
-                echo view('protokolle/protokollKapitelID1View', $datenInhalt);
+                echo view('protokolle/FLUGZEUG_EINGABE_View', $datenInhalt);
             break;
             case PILOT_EINGABE:    
-                echo view('protokolle/protokollKapitelID2View', $datenInhalt);
+                echo view('protokolle/PILOT_EINGABE_View', $datenInhalt);
             break;
             case BELADUNG_EINGABE:
-                echo view('protokolle/protokollKapitelID3View', $datenInhalt);
+                echo view('protokolle/BELADUNG_EINGABE_View', $datenInhalt);
                 unset($_SESSION['protokoll']['beladungszustand']);
             break;
             default:
                 echo view('protokolle/protokollKapitelView', $datenInhalt);
         }
-    }
-    
-    protected function zeigeWarteSeite()
-    {
-        echo view('templates/headerView');
-        echo view('templates/wartenView');
-        echo view('templates/footerView');
     }
 }

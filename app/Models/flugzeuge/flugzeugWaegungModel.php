48c8934e0c32a1a291c782ec36abd38f91f437fa
Lars Kastner <lars.kastner@dlr.de>
2022-05-13 15:20:18 +0200

    Bugfix: getFlugzeugWaegungNachFlugzeugIDUndDatum():Falls keine Wägung vor Zeitpunkt des Protokolls, die dem Zeitpunkt nächste Wägung nehmen

--- app/Models/flugzeuge/flugzeugWaegungModel.php
+++ app/Models/flugzeuge/flugzeugWaegungModel.php
@@ -19,81 +19,81 @@ class flugzeugWaegungModel extends Model
 
     protected $allowedFields 	= ['flugzeugID', 'leermasse', 'schwerpunkt', 'zuladungMin', 'zuladungMax', 'datum'];
 
-        /**
-        * Diese Funktion ruft nur die Flugzeugdetails mit
-        * der jeweiligen flugzeugID auf
-        *
-        * @param  mix $id int oder string
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft nur die Flugzeugdetails mit
+    * der jeweiligen flugzeugID auf
+    *
+    * @param  mix $id int oder string
+    * @return array
+    */
     public function getFlugzeugWaegungenNachFlugzeugID($flugzeugID)
     {			
         return($this->where('flugzeugID', $flugzeugID)->first());
     }
     
-        /**
-        * Diese Funktion ruft nur die Flugzeugdetails mit
-        * der jeweiligen flugzeugID auf
-        *
-        * @param  mix $id int oder string
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft nur die Flugzeugdetails mit
+    * der jeweiligen flugzeugID auf
+    *
+    * @param  mix $id int oder string
+    * @return array
+    */
     public function getAlleWaegungenNachFlugzeugID($flugzeugID)
     {			
         return($this->where('flugzeugID', $flugzeugID)->orderBy('datum', 'ASC')->findAll());
     }
 
-        /**
-        * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "variometer"
-        * gespeichert wurden. Dabei werden Dopplungen ignoriert
-        *
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "variometer"
+    * gespeichert wurden. Dabei werden Dopplungen ignoriert
+    *
+    * @return array
+    */
     public function getFlugzeugDetailsDistinctVariometerEingaben()
     {
         return $this->distinct()->findColumn('variometer');
     }
 
 
-        /**
-        * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "tek"
-        * gespeichert wurden. Dabei werden Dopplungen ignoriert
-        *
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "tek"
+    * gespeichert wurden. Dabei werden Dopplungen ignoriert
+    *
+    * @return array
+    */
     public function getFlugzeugDetailsDistinctTekEingaben()
     {
         return $this->distinct()->findColumn('tek');
     }
 
-        /**
-        * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "pitotPosition"
-        * gespeichert wurden. Dabei werden Dopplungen ignoriert
-        *
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "pitotPosition"
+    * gespeichert wurden. Dabei werden Dopplungen ignoriert
+    *
+    * @return array
+    */
     public function getFlugzeugDetailsDistinctPitotPositionEingaben()
     {
         return $this->distinct()->findColumn('pitotPosition');
     }
 
-        /**
-        * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "bremsklappen"
-        * gespeichert wurden. Dabei werden Dopplungen ignoriert
-        *
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "bremsklappen"
+    * gespeichert wurden. Dabei werden Dopplungen ignoriert
+    *
+    * @return array
+    */
     public function getFlugzeugDetailsDistinctBremsklappenEingaben()
     {
         return $this->distinct()->findColumn('bremsklappen');
     }
 
-        /**
-        * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "bezugspunkt"
-        * gespeichert wurden. Dabei werden Dopplungen ignoriert
-        *
-        * @return array
-        */
+    /**
+    * Diese Funktion ruft alle Eingaben auf, die jemals in der Spalte "bezugspunkt"
+    * gespeichert wurden. Dabei werden Dopplungen ignoriert
+    *
+    * @return array
+    */
     public function getFlugzeugDetailsDistinctBezugspunktEingaben()
     {
         return $this->distinct()->findColumn('bezugspunkt');
@@ -102,7 +102,15 @@ class flugzeugWaegungModel extends Model
     public function getFlugzeugWaegungNachFlugzeugIDUndDatum($flugzeugID, $datum)
     {
         $query = "SELECT * FROM flugzeug_waegung WHERE flugzeugID = " . $flugzeugID . " AND datum <= '" . $datum . "' ORDER BY datum DESC";
-        return $this->query($query)->getResultArray()[0];
+        $ergebnis = $this->query($query)->getResultArray();
+        
+        if(empty($ergebnis))
+        {
+            $queryNeu = "SELECT * FROM flugzeug_waegung WHERE flugzeugID = ". $flugzeugID ." ORDER BY ABS( DATEDIFF('". date('Y-m-d', strtotime($datum)) ."', NOW() ) ), id DESC LIMIT 1";   
+            $ergebnis =  $this->query($queryNeu)->getResultArray();
+        }
+        
+        return $ergebnis ?? NULL;
     }
     
     public function getSpaltenInformationen()

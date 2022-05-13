2e077e0a0615f628f4930a7cccd9aec2ce667d91
Lars Kastner <lars.kastner@dlr.de>
2022-05-13 13:16:00 +0200

    Zwischenstand

--- app/Controllers/protokolle/protokolldateninhaltcontroller.php
+++ app/Controllers/protokolle/protokolldateninhaltcontroller.php
@@ -13,6 +13,10 @@ use App\Models\protokolle\{ datenModel };
  */
 class Protokolldateninhaltladecontroller extends Protokollcontroller 
 {
+    /**
+     * Gibt die Anzahl der Dateneingabefelder bei einem Multiplen Inputfeld an, wenn in der Datenbank kein anderer Wert vorhanden ist.
+     */
+    const STANDARDANZAHL_MULTIPELFELDER = 10;
     
     /**
      * Entscheidet, welche zusätzlichen Informationen geladen werden müssen.
@@ -54,13 +58,14 @@ class Protokolldateninhaltladecontroller extends Protokollcontroller
                 break;
             default:
                 $inhaltZusatz = [
-                    'eingabenDatenArray'        => $this->ladeProtokollEingaben(),
-                    'inputsDatenArray'          => $this->ladeProtokollInputs(),
-                    'auswahllistenDatenArray'   => $this->ladeAuswahllisten(),
-                    'kommentarFeldNotwendig'    => $this->pruefeAufKommentarFeld(),
-                    'hStFeldNotwendig'          => $this->pruefeAufHStWegFeld($this->ladeProtokollInputs()),
-                    'anzahlMultipelFelder'      => $this->pruefeAufMultipelFelder($this->ladeProtokollEingaben()),
+                    'eingabenDatenArray'                    => $this->ladeProtokollEingaben(),
+                    'inputsDatenArray'                      => $this->ladeProtokollInputs(),
+                    'kommentarFeldNotwendig'                => $this->pruefeAufKommentarFeld(),
                 ];
+                
+                $inhaltZusatz['auswahllistenDatenArray']    = $this->ladeAuswahloptionen($inhaltZusatz['inputsDatenArray']);
+                $inhaltZusatz['hStFeldNotwendig']           = $this->pruefeAufHStWegFeld($inhaltZusatz['inputsDatenArray']);
+                $inhaltZusatz['anzahlMultipelFelder']       = $this->pruefeAufMultipelFelder($inhaltZusatz['eingabenDatenArray']);
         }
         
         return $inhaltZusatz;
@@ -184,34 +189,46 @@ class Protokolldateninhaltladecontroller extends Protokollcontroller
      * Lädt die protokollEingaben des aktuellen Kapitels und/oder Unterkapitels aus der Datenbank.
      * 
      * Lade eine Instanz des protokollEingabenModels.
-     * Initialisiere das $temporaereEingabeArray.
+     * Initialisiere das $protokollEingabenArray.
      * Für jedes Unterkapitel, des aktuellen Kapitels (wenn kein Unterkapitel vorhanden dann $protokollUnterkapitelID == 0) lade für
      * jede protokollEingabe des aktuellen Unterkapitels die protokollEingabeDaten nach der protokollEingabeID.
-     * Speichere die protokollEingaben jeweils mit der protokollEingabenID als Index im $temporaeresEingabeArray.
-     * Gib das $temporaereEingabeArray zurück, wenn alle protokollEingaben erfasst wurden.
+     * Speichere die protokollEingaben jeweils mit der protokollEingabenID als Index im $protokollEingabenArray.
+     * Gib das $protokollEingabenArray zurück, wenn alle protokollEingaben erfasst wurden.
      * 
-     * @return array $temporaeresEingabeArray
+     * @return array $protokollEingabenArray
      */
     protected function ladeProtokollEingaben() 
     {
         $protokollEingabenModel     = new protokollEingabenModel();       
-        $temporaeresEingabeArray    = array();
+        $protokollEingabenArray     = array();
 
         foreach($_SESSION['protokoll']['protokollLayout'][$_SESSION['protokoll']['aktuellesKapitel']] as $protokollUnterkapitelID => $unterkapitel)
         {
             foreach($_SESSION['protokoll']['protokollLayout'][$_SESSION['protokoll']['aktuellesKapitel']][$protokollUnterkapitelID] as $protokollEingabeID => $eingaben)
             {
-                $temporaeresEingabeArray[$protokollEingabeID] = $protokollEingabenModel->getProtokollEingabeNachID($protokollEingabeID);
+                $protokollEingabenArray[$protokollEingabeID] = $protokollEingabenModel->getProtokollEingabeNachID($protokollEingabeID);
             }
         }
         
-        return $temporaeresEingabeArray;            
+        return $protokollEingabenArray;            
     }
     
+    /**
+     * Lädt die protokollInputs des aktuellen Kapitels und/oder Unterkapitels für jede protokollEingabe aus der Datenbank. 
+     * 
+     * Lade eine Instanz des protokollInputsMitInputTypModel.
+     * Initialisiere das $protokollInputsArray.
+     * Für jedes Unterkapitel, des aktuellen Kapitels (wenn kein Unterkapitel vorhanden dann $protokollUnterkapitelID == 0) lade 
+     * jede protokollEingabe. Für jede protokollEingabe lade jeden protokollInput.
+     * Speichere die protokollInputs jeweils mit der protokollInputID als Index im $protokollInputsArray.
+     * Gib das $protokollInputsArray zurück, wenn alle protokollInputs erfasst wurden.
+     * 
+     * @return array $protokollInputsArray
+     */
     protected function ladeProtokollInputs()
     {
         $protokollInputsMitInputTypModel    = new protokollInputsMitInputTypModel();            
-        $temporaeresInputArray              = array();
+        $protokollInputsArray               = array();
 
         foreach($_SESSION['protokoll']['protokollLayout'][$_SESSION['protokoll']['aktuellesKapitel']] as $protokollUnterkapitelID => $unterkapitel)
         {
@@ -219,48 +236,71 @@ class Protokolldateninhaltladecontroller extends Protokollcontroller
             {
                 foreach($_SESSION['protokoll']['protokollLayout'][$_SESSION['protokoll']['aktuellesKapitel']][$protokollUnterkapitelID][$protokollEingabeID] as $protokollInputID => $inputs)
                 {
-                    $temporaeresInputArray[$protokollInputID] = $protokollInputsMitInputTypModel->getProtokollInputMitInputTypNachProtokollInputID($protokollInputID);
+                    $protokollInputsArray[$protokollInputID] = $protokollInputsMitInputTypModel->getProtokollInputMitInputTypNachProtokollInputID($protokollInputID);
                 }
             }
         }
-        
-        return $temporaeresInputArray;  
+
+        return $protokollInputsArray;  
     }
     
-    protected function ladeAuswahllisten() 
+    /**
+     * Lädt die Optionen der Auswahllisten für Inputs mit dem InputTyp "Auswahloptionen".
+     * 
+     * Lade eine Instanz eds auswahllistenModels.
+     * Initialisiere das $auswahllistenArray.
+     * Für jeden protokollInput, prüfe, ob der inputTyp "Auswahloptionen" lautet. Wenn ja, lade die entsprechenden Optionen für diesen Input
+     * aus der Datenbank und speichere sie im $auswahllistenArray mit der protokollInputID als Index.
+     * Gib das $auswahllistenArray zurück.
+     * 
+     * @param array $protokollInputArray
+     * @return array $auswahllistenArray
+     */
+    protected function ladeAuswahloptionen(array $protokollInputArray) 
     {
-        $auswahllistenModel             = new auswahllistenModel();             
-        $temporaeresAuswahllistenArray  = array();
+        $auswahllistenModel = new auswahllistenModel();             
+        $auswahllistenArray = array();
         
-        foreach($this->ladeProtokollInputs() as $protokollInput)
-        {
-            
+        foreach($protokollInputArray as $protokollInput)
+        {            
             if($protokollInput["inputTyp"] === "Auswahloptionen")
             {
-                $temporaeresAuswahllistenArray[$protokollInput["id"]] = $auswahllistenModel->getAuswahllisteNachProtokollInputID($protokollInput["id"]);
+                $auswahllistenArray[$protokollInput["id"]] = $auswahllistenModel->getAuswahllisteNachProtokollInputID($protokollInput["id"]);
             }   
         }
         
-        return $temporaeresAuswahllistenArray;
+        return $auswahllistenArray;
     }
     
+    /**
+     * Prüft, ob das aktuelle Kapitel ein Kommentarfeld erhält.
+     * 
+     * Lade eine Instanz des protokollKapitelModels.
+     * Wenn der Eintrag des Kapitels mit der aktuellen <protokollKapitelID> in der Datenbanktabelle protokoll_Kapitel eine "1" 
+     * in der Spalte "kommentar" besitzt, dann gib TRUE zurück. Sonst FALSE. 
+     * 
+     * @return boolean
+     */
     protected function pruefeAufKommentarFeld()
     {
-        $protokollKapitelModel = new protokollKapitelModel();
-        
-        if($protokollKapitelModel->getProtokollKapitelNachID($_SESSION['protokoll']['kapitelIDs'][$_SESSION['protokoll']['aktuellesKapitel']])['kommentar'] == 1)
-        {
-            return TRUE;
-        }
-        
-        return FALSE;
+        $protokollKapitelModel = new protokollKapitelModel();       
+        return $protokollKapitelModel->getProtokollKapitelNachID($_SESSION['protokoll']['kapitelIDs'][$_SESSION['protokoll']['aktuellesKapitel']])['kommentar'] ? TRUE : FALSE;
     }
     
-    protected function pruefeAufHStWegFeld(array $inputsArray)
+    /**
+     * Prüft, ob einer der übergebenen protokollInputs eine Höhensteuerangabe benötigt.
+     * 
+     * Wenn einer der übergebenen protokollInputs einen Wert != 0 im Index 'hStWeg' besitzt, gib TRUE zurück. Sonst FALSE.
+     * 
+     * @param array $protokollInputsArray
+     * @return boolean
+     */
+    protected function pruefeAufHStWegFeld(array $protokollInputsArray)
     {
-        foreach($inputsArray as $input)
+        foreach($protokollInputsArray as $protokollInput)
         {
-            if($input['hStWeg']){
+            if($protokollInput['hStWeg'])
+            {
                 return TRUE;
             }
         }
@@ -268,34 +308,57 @@ class Protokolldateninhaltladecontroller extends Protokollcontroller
         return FALSE;
     }
     
-    protected function pruefeAufMultipelFelder(array $eingabenArray)
+    /**
+     * Prüft, ob einer der übergebenen protokollEingaben eine multiple Dateneingabe erfordert und gibt ggf. die Anzahl der Felder zurück.
+     * 
+     * Lade eine Instanz des protokollLayoutsModels.
+     * Initialisiere das $multipelFelderArray.
+     * Prüfe, ob eine der übergebenen protokolLEingaben einen mulitplen Input enthält. Falls ja, Initialisiere die Variablen
+     * $multipelFelderArray mit der protokollEingabeID als Index und $anzahlMultipelFelder.
+     * Für alle protokollInputs dieser multiplen protokollEingabe, lade die Anzahl der im Zwischenspeicher vorhandenen Anzahl an ausgefüllten
+     * Inputs. Wenn diese Anzahl größer ist, als der im $multipelFelderArray zu dieser protokollEingabeID gespeicherte Wert, ersetze ihn durch den neuen.
+     * Wenn keine eingegebenen Werte im Zwischenspeicher vorhanden sind und damit $multipelFelderArray == NULL, dann setze die Variable 
+     * zu STANDARDANZAHL_MULTIPELFELDER (Konstante in dieser Klasse). Sonst mache nichts.
+     * Gib das $multipelFelderArray zurück.
+     * 
+     * @param array $protokollEingabenArray
+     * @return array $multipelFelderArray
+     */
+    protected function pruefeAufMultipelFelder(array $protokollEingabenArray)
     {
-        $protokolleLayoutsModel = new protokollLayoutsModel();
+        $protokolleLayoutsModel = new protokollLayoutsModel();       
+        $multipelFelderArray    = array();
         
-        $multipelFelderArray = array();
-        
-        foreach($eingabenArray as $protokollEingabeID => $eingabe)
+        foreach($protokollEingabenArray as $protokollEingabeID => $protokollEingabe)
         {
-            if($eingabe['multipel'])
+            if($protokollEingabe['multipel'])
             {                
-                $multipelFelderArray[$protokollEingabeID] = NULL;
-                
-                $anzahlMultipelFelder = NULL;
+                $multipelFelderArray[$protokollEingabeID]   = NULL;                
+                $anzahlMultipelFelder                       = NULL;
 
                 foreach($protokolleLayoutsModel->getInputIDsNachProtokollEingabeID($protokollEingabeID) as $protokollInputID)
                 {
-                    $anzahlMultipelFelder = $this->ladeAnzahlMultipelInputs($protokollInputID['protokollInputID']);
+                    $anzahlMultipelFelder = $this->ladeAnzahlAusgefuellteMultipelInputs($protokollInputID['protokollInputID']);
                     $anzahlMultipelFelder > $multipelFelderArray[$protokollEingabeID] ? $multipelFelderArray[$protokollEingabeID] = $anzahlMultipelFelder : NULL;
                 }
 
-                empty($multipelFelderArray[$protokollEingabeID]) ? $multipelFelderArray[$protokollEingabeID] = 10 : NULL;     
+                empty($multipelFelderArray[$protokollEingabeID]) ? $multipelFelderArray[$protokollEingabeID] = STANDARDANZAHL_MULTIPELFELDER : NULL;     
             }
         }
 
         return $multipelFelderArray; 
     }
     
-    protected function ladeAnzahlMultipelInputs(int $protokollInputID)
+    /**
+     * Prüft, ob für die übermittelte $protokollInputID Werte im Zwischenspeicher vorhanden sind und gibt die Anzahl zurück.
+     * 
+     * Wenn im Zwischenspeicher keine eingegebenen Werte mit der übermittelten protokollInputID vorhanden sind, gib sofort NULL zurück.
+     * Andernfalls initialisiere die Variable $anzahlMultipelFelder und setze sie zu NULL.
+     * 
+     * @param int $protokollInputID
+     * @return int $anzahlMultipelFelder
+     */
+    protected function ladeAnzahlAusgefuellteMultipelInputs(int $protokollInputID)
     {
         if(empty($_SESSION['protokoll']['eingegebeneWerte'][$protokollInputID]))
         {
@@ -308,7 +371,7 @@ class Protokolldateninhaltladecontroller extends Protokollcontroller
         {
             foreach($wertRichtungMultipelNr[0] as $mulitpelNr => $wert)
             {
-                $mulitpelNr  > $anzahlMultipelFelder ? $anzahlMultipelFelder = $mulitpelNr : NULL;               
+                $mulitpelNr > $anzahlMultipelFelder ? $anzahlMultipelFelder = $mulitpelNr : NULL;               
             }
         }
         

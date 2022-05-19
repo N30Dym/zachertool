40be092dbd991277480544d62480be4aa47e5c96
Lars Kastner <lars.kastner@dlr.de>
2022-05-19 12:52:29 +0200

    Nachricht wird nicht mehr escaped, um HTML-Befehle einfügen zu können

--- app/Views/templates/nachrichtView.php
+++ app/Views/templates/nachrichtView.php
@@ -4,7 +4,7 @@
 <main>
     <div class="min-vh-100 h-100"></div>
     <div style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);" class="text-center">	
-        <h3><?= esc($nachricht) ?></h3>
+        <h3><?= $nachricht ?></h3>
         <a href="<?= esc($link) ?>"
             <button type="button" class="btn btn-primary">Zurück</button>
         </a>
